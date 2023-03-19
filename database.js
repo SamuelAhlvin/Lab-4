62339f598fd5f3783033bce05ff13e6bb9fc4ef7
SamuelAhlvin <ahlvinsamme@gmail.com>
2023-03-19 19:55:49 +0100

    Token works for admin page

--- database.js
+++ database.js
@@ -8,9 +8,19 @@ const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE, (err) =
 //Check if username exists in database
 async function userExists(username) {
   return new Promise((resolve, reject) => {
-    db.all(`SELECT * FROM users WHERE username = $username `, { $username: username }, (error, rows) => {
+    db.all(`SELECT * FROM users WHERE name = $username `, { $username: username }, (error, rows) => {
       if (error) reject(error)
       else resolve(rows.length > 0)
+    })
+  })
+}
+
+//Gets all user info
+async function dbData() {
+  return new Promise((resolve, reject) => {
+    db.all(`SELECT * FROM users`, (error, rows) => {
+      if (error) reject(error)
+      else resolve(rows)
     })
   })
 }
@@ -18,7 +28,7 @@ async function userExists(username) {
 //Gets encrypted password from username
 async function validateLogin(username) {
   return new Promise((resolve, reject) => {
-    db.get(`SELECT password FROM users WHERE username = $username`, { $username: username }, (error, row) => {
+    db.get(`SELECT password FROM users WHERE name = $username`, { $username: username }, (error, row) => {
       if (error) reject(error)
       else resolve(row.password)
     })
@@ -27,7 +37,7 @@ async function validateLogin(username) {
 
 //Register user
 async function registerUser(username, password) {
-  const sql = `INSERT INTO users (username, password) VALUES ($username, $password)`
+  const sql = `INSERT INTO users (name, password) VALUES ($username, $password)`
   const params = { $username: username, $password: password }
 
   return new Promise((resolve, reject) => {
@@ -48,5 +58,6 @@ async function registerUser(username, password) {
 module.exports = {
   userExists,
   registerUser,
-  validateLogin
+  validateLogin,
+  dbData,
 }
--- server.js
+++ server.js
@@ -9,17 +9,19 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
-var dbEncryption
+let currentKey = null;
+let isAdmin = null;
+let dbData = null;
 
 app.get('/', (req, res) => {
-  res.redirect("/LOGIN")
+  res.redirect("/identify")
 })
 
-app.get('/LOGIN', (req, res) => {
+app.get('/identify', (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/LOGIN', async (req, res) => {
+app.post('/identify', async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -34,59 +36,55 @@ app.post('/LOGIN', async (req, res) => {
   if (userExists) {
     var encPass = await db.validateLogin(username)
 
-    //Decrypts password from database
-    bcrypt.compare(password, encPass, (err, result) => {
-      if (err) {
-        console.error(err.message);
-        res.render('fail.ejs');
-      } else if (result) {
-        //Creates jwt and logs it
-        const token = jwt.sign({ username: username }, process.env.TOKEN);
-        console.log(token);
+    if (encPass == password) {
+      isAdmin = username == "admin"
+      const token = jwt.sign(username, process.env.TOKEN);
+      currentKey = token;
+      res.redirect("/GRANTED")
+    } else {
+      res.render('fail.ejs');
+    }
 
-        res.render('start.ejs');
-      } else {
-        res.render('fail.ejs');
-      }
-    });
   } else {
+    console.log("User does not exist")
     res.render('fail.ejs')
   }
 })
 
-app.post('/REGISTER', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  //Checks if the input fields are valid
-  if (!username || !password) {
-    return res.status(400).send('Please provide a username and password')
-  }
-
-  //Encrypts password and saves in database
-  bcrypt.hash(password, 10, async (err, hash) => {
-    if (err) {
-      console.error(err.message);
-      res.redirect('/register');
-    } else {
-      try {
-        await db.registerUser(username, hash)
-        res.redirect("/LOGIN")
-      } catch (error) {
-        console.log(error.message);
-        res.render('fail.ejs', { error: 'Username already taken' })
-      }
-    }
-  });
-})
-
 function authenticateToken(req, res, next) {
-  console.log('Authentication in progress')
-  next()
+  if (currentKey == "") {
+    res.redirect("/identify")
+  } else if (jwt.verify(currentKey, process.env.TOKEN)) {
+    next();
+  } else {
+    res.redirect("/identify")
+  }
 }
 
-app.get('/REGISTER', (req, res) => {
-  res.render('register.ejs')
+app.get('/GRANTED', authenticateToken, async (req, res) => {
+
+  if (isAdmin) {
+    res.redirect("/admin");
+  } else {
+    res.render('start.ejs');
+  }
+})
+
+app.get('/admin', async (req, res) => {
+  dbData = await db.dbData();
+  res.render('admin.ejs', { dbData });
+})
+
+app.get('/student1', (req, res) => {
+  res.render('student1.ejs')
+})
+
+app.get('/student2', (req, res) => {
+  res.render('student2.ejs')
+})
+
+app.get('/teacher', (req, res) => {
+  res.render('teacher.ejs')
 })
 
 app.listen(5000, () => {
